85506932d6e325e4439a2f92b84139bc0200de29
suneelsuthar <suneelsuther123@gmail.com>
2024-11-20 17:11:06 +0500

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2eXZRrpUy_0GblM5EgWb_TE9oW5TppbY",
-  authDomain: "webdevelopment-6183d.firebaseapp.com",
-  databaseURL: "https://webdevelopment-6183d-default-rtdb.firebaseio.com",
-  projectId: "webdevelopment-6183d",
-  storageBucket: "webdevelopment-6183d.appspot.com",
-  messagingSenderId: "1048137878068",
-  appId: "1:1048137878068:web:80f14b0952df5237a69861",
-  measurementId: "G-E09YGE3XQN",
+  //   user your own credentials
 };
 
 // Initialize Firebase
